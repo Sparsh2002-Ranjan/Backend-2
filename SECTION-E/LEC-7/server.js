93cc773c3fa52de2f19391584343c818d13c03e6
Sparsh Ranjan <ranjansparsh872@gmail.com>
2025-02-01 15:39:54 -0800

    Frontend

--- SECTION-E/LEC-7/server.js
+++ SECTION-E/LEC-7/server.js
@@ -5,14 +5,14 @@ const path = require('path');
 const app = express();
 
 app.use(methodOverride('_method'));
-app.use(express.urlencoded({extended:true}))
-app.use(express.static(path.join(__dirname, 'static')));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'static')));  // Ensure static files are served correctly
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.use('/u1', userRoutes)
+app.use('/u1', userRoutes);
 
-app.listen(3000, ()=>{
-    console.log('server is running at port 3000')
-})
+app.listen(3000, () => {
+    console.log('server is running at http://localhost:3000/u1/user/all');
+});
